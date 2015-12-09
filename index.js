e7f3d5b46f06b31d78960c9b6b8aa908fd0353d9
Dominic Tarr <dominic.tarr@gmail.com>
2015-12-09 23:24:26 +1300

    rewrite for correct abort behaviour

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
-var pull = require('pull-stream')
+module.exports = function (onClose) {
+  var buffer = [], ended, abort, cb
 
-module.exports = pull.Source(function (onClose) {
-  var buffer = [], cbs = [], waiting = [], ended
+  function callback (err, val) {
+    var _cb = cb
+    if(err && onClose) {
+      var c = onClose
+      onClose = null
+      c(err === true ? null : err)
+    }
+    cb = null
+    _cb(err, val)
+
+  }
 
   function drain() {
-    var l
-    while(waiting.length && ((l = buffer.length) || ended)) {
-      var data = buffer.shift()
-      var cb   = cbs.shift()
-      waiting.shift()(l ? null : ended, data)
-      cb && cb(ended === true ? null : ended)
+    if(!cb) return
+
+    if(abort)                        callback(abort)
+    else if(!buffer.length && ended) callback(ended)
+    else if(buffer.length)           callback(null, buffer.shift())
+  }
+
+  function read (_abort, _cb) {
+    if(_abort) {
+      abort = _abort
+      //if there is already a cb waiting, abort it.
+      if(cb) callback(abort)
     }
-  }
-
-  function read (end, cb) {
-    ended = ended || end
-    waiting.push(cb)
-    drain()
-    if(ended)
-      onClose && onClose(ended === true ? null : ended)
-  }
-
-  read.push = function (data, cb) {
-    if(ended)
-      return cb && cb(ended === true ? null : ended)
-    buffer.push(data); cbs.push(cb)
+    cb = _cb
     drain()
   }
 
-  read.end = function (end, cb) {
-    if('function' === typeof end)
-      cb = end, end = true
+  read.push = function (data) {
+    if(ended) return
+    buffer.push(data)
+    drain()
+  }
+
+  read.end = function (end) {
     ended = ended || end || true;
-    if(cb) cbs.push(cb)
     drain()
-    if(ended)
-      onClose && onClose(ended === true ? null : ended)
   }
 
   return read
-})
+}
+
 
